91feab535ace92b1218222ec04de9df3bdb165fc
Alan Cameron Wills <alan@cameronwills.org>
2026-05-02 13:57:34 +0100

    older version of node

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $BASH_SOURCE`
 sleep 30s
-( ./fetch-code.sh; npm start ) > server.log &
+( ./fetch-code.sh; npm start ) &> server.log &
 (sleep 25s; chromium-browser --kiosk --no-first-run --disable-infobars --disable-pinch --start-fullscreen --disk-cache-size=1 http://localhost:6345/pv.html?nocursor) 
 read -t 20 -p "--"
