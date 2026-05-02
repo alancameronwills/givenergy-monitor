569cb2ac8f0fadce73d375bb1f11efbeef6868ca
Alan Cameron Wills <alan@cameronwills.org>
2026-05-02 13:17:21 +0100

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd `dirname $BASH_SOURCE`
-./fetch-code.sh
-npm start &
-(sleep 5s; chromium-browser --kiosk --no-first-run --disable-infobars --disable-pinch --start-fullscreen --disk-cache-size=1 http://localhost:6345/pv.html/?nocursor) 
-read -t 30 -p "--"
+sleep 30s
+( ./fetch-code.sh; npm start ) > server.log &
+(sleep 25s; chromium-browser --kiosk --no-first-run --disable-infobars --disable-pinch --start-fullscreen --disk-cache-size=1 http://localhost:6345/pv.html?nocursor) 
+read -t 20 -p "--"
